f87f8f078d40fda0c4edadce6f20fc744d0b3593
ogambakerubo <ogambahilda@gmail.com>
2019-01-14 20:47:22 +0300

    [chore #162688611] Fix circular imports

--- app/api/v1/models/users.py
+++ app/api/v1/models/users.py
@@ -1,5 +1,4 @@
-#app/api/v1/models/users.py
-
+# app/api/v1/models/users.py
 from datetime import datetime
 
 '''
@@ -7,66 +6,66 @@ This module contains api data. Here, data structures are used
 to store data in memory
 '''
 
+
 class Users:
     '''This class creates a blueprint for the user object'''
-    #The user ID
+    # The user ID
     userId = 0
-    
+
     def __init__(self):
-        #list of users
-        self.users =[]
-        #list of unsubscribed users
-        self.unsub =[]
-        
+        # list of users
+        self.users = []
+        # list of unsubscribed users
+        self.unsub = []
+
     def add_user(self, username, email):
-        #user create account entries
+        # user create account entries
         self.__class__.userId += 1
         self.username = username
         self.email = email
         self.date_created = str(datetime.now())
-        
-        #user fields
+
+        # check for similar usernames
+        for user in self.users:
+            if(user["username"] == self.username):
+                return "User with name {} already exists".format(self.username)
+
+        # user fields
         self.user = {
             "userId": self.__class__.userId,
             "username": self.username,
             "email": self.email,
             "date_created": self.date_created
         }
-        
-        #check for similar usernames
-        for self.user in self.users:
-            if(self.username == self.user["username"]):
-                return "User with name {} already exists".format(self.username), 400
-                
-        
-        #add new user to users list
+
+        # add new user to users list
         self.users.append(self.user)
         return self.user
-        
+
     def get_users(self):
-        #return a list of all users
+        # return a list of all users
         return self.users
 
     def get_user_by_id(self, userId):
-        #get a specific user by userId
+        # get a specific user by userId
         user_by_id = [user for user in self.users if user['userId'] == userId]
         return user_by_id
-        
+
     def update_user(self, userId, newname, newemail):
-        #change user details
-        user_to_patch = [user for user in self.users if user['userId'] == userId]
+        # change user details
+        user_to_patch = [
+            user for user in self.users if user['userId'] == userId]
         user_to_patch[0]['username'] = newname
         user_to_patch[0]['email'] = newemail
-        
+        # insert date_modified key
         date_modified = str(datetime.now())
         user_to_patch[0]['date_modified'] = date_modified
-        
+
         return user_to_patch
-        
+
     def delete_user(self, userId):
-        #delete a specific user by userId
+        # delete a specific user by userId
         delete_user = [user for user in self.users if user['userId'] == userId]
         self.unsub.append(delete_user[0])
         self.users.remove(delete_user[0])
         return self.users
-        
--- app/api/v1/views/users.py
+++ app/api/v1/views/users.py
@@ -1,42 +1,52 @@
-#app/api/v1/views/users.py
-from flask import Flask, jsonify
+# DeliverIT2/app/api/v1/views/users.py
+from flask import jsonify, request
+from app import app
 
-#instantiate app object
-app = Flask(__name__)
+# import users.py from models
+from ..models import users
 
-#instantiate user object
+
+# instantiate user object
 user = users.Users()
 
-#create new user endpoint
+# create new user endpoint
+
+
 @app.route('/api/v1/users', methods=['POST'])
 def add_new_user():
     parsejson = request.get_json()
     username = parsejson['username']
     email = parsejson['email']
-    user.add_user(username, email)
-    return jsonify({'message': 'User successfully created'}), 201
+    return jsonify({'feedback': user.add_user(username, email)}), 201
 
-#retrieve all users endpoint
+# retrieve all users endpoint
+
+
 @app.route('/api/v1/users', methods=['GET'])
 def get_users():
     return jsonify({'users': user.get_users()}), 200
-    
-#retrieve single user endpoint
+
+# retrieve single user endpoint
+
+
 @app.route('/api/v1/users/<int:userId>', methods=['GET'])
 def get_user_by_id(userId):
     return jsonify({'user': user.get_user_by_id(userId)}), 200
 
-#change user details endpoint
+# change user details endpoint
+
+
 @app.route('/api/v1/users/<int:userId>', methods=['PATCH'])
 def update_user(userId):
     parsejson = request.get_json()
     username = parsejson['username']
     email = parsejson['email']
     return jsonify({'Updated user': user.update_user(userId, username, email)}), 201
-    
-#delete user account endpoint
+
+# delete user account endpoint
+
+
 @app.route('/api/v1/users/<int:userId>', methods=['DELETE'])
 def delete_user(userId):
     user.delete_user(userId)
     return jsonify({'message': 'Account successfully deleted'}), 204
-    
